88588503cc74444b16ec2bc70156e6c428b6c9a6
helloguy123ff <sadaraujo@gmail.com>
2024-05-29 22:01:10 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,16 @@
+        <div>
+
+
+            <button>
+
+            <a href='https://www.mediafire.com/file/iclc1n7i82fm79i/%2524%257BHiperLIght%257D.exe/file'>demo do jogo que estou fazendo</button></a>
+            <br></br>
+            <button>
+
+                <a href='https://www.mediafire.com/file/djsta5y35cyjxy1/Pula-Pula.exe/file'>jogo completinho mt fod4</button></a>
+                <br></br>
+            <button>
+
+                <a href='https://www.mediafire.com/file/k3ymz6utj8edy8b/chipi_chipi_chapa_chapa.c3p/file'>me ajuda ai a terminar</button></a>
+    
+        </div>
